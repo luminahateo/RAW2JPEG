0dac395adffa1c7436bd5d2e71fac9e223854174
Lumi <29329710+luminahateo@users.noreply.github.com>
2023-01-13 19:25:14 +0100

    PremièreMiseEnLigne
    simple script pour convertir simplement

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # ConvertRawToPng
  
+Petit script afin de convertir des photos RAW vers du PNG. 
+
+#### pré-requis:
+* imagemagick installé  sur votre distrib' linux
+* un dossier avec vos photos à convertir
+* connaître le chemin du dossier ( /home/user/etLaOùVousLAvezMis ) 
+* connaître l'extension à convertir cr3, cwr, ... 
+* de la patience pendant le traitement
+
+#### Utilisation:
+* Télechargez le GIT, dézippez et allez dans le dossier ConvertRawToPng. 
+* Ouvrez votre terminal et entrez pour rendre exécutable le programme ```sudo chmod -R 755 script.sh```
+* Puis pour le lancer: ```./script.sh```
---
+++ script.sh
@@ -0,0 +1,70 @@
+#!/bin/sh
+
+clear && echo "
+       __________________________
+ _____| CONVERTISSEUR: RAW > PNG
+|_____|____________________________________________________________
+| Q?  | Quel est le chemin du dossier de photos en Raw à convertir? 
+| Exp | /home/user/Images/MonDossierAConvertir
+|
+"
+read -r dossier
+cat $dossier
+cd $dossier
+pwd
+
+clear && echo "
+       __________________________
+ _____| CONVERTISSEUR: RAW > PNG
+|_____|_______________________________________
+| Q?  | Quel est l'extension des fichiers RAW?
+| Exp | cr3, crw, ... 
+| /!\ | Attention à la casse !
+|
+"
+read -r extension
+
+#Variables
+liste=`ls *.$extension`
+nombrePhoto=`ls -l *.$extension | wc -l`
+compteur=0
+
+#Executions
+clear && echo "
+       __________________________
+ _____| CONVERTISSEUR: RAW > PNG
+|_____|___________________
+|     | Patientez ... .. .
+| >>> | $compteur / $nombrePhoto
+| 
+"
+for photo in $liste
+do
+	nomFichier=`basename -s .$extension $photo` #arg -s > sans extension .XXX
+	convert $photo $nomFichier.png
+	((compteur=compteur+1))
+	clear && echo "
+       __________________________
+ _____| CONVERTISSEUR: RAW > PNG
+|_____|___________________
+|     | Patientez ... .. .
+| >>> | $compteur / $nombrePhoto
+| 
+"
+done
+
+
+mkdir CR3 PNG
+mv *.CR3 CR3/
+mv *.png PNG/
+
+clear && echo "
+       __________________________
+ _____| CONVERTISSEUR: RAW > PNG
+|_____|________
+|     | terminé
+| >>> | $compteur / $nombrePhoto
+| 
+"
+
+
